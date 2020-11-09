b3c2830182942adf9c38fc4e3bf324a054257b5d
dzzMy <1624560307@qq.com>
2020-11-09 10:09:24 +0800

    Array的两数之和hash求解

--- array/src/main/java/com/hqeducation/TwoSum.java
+++ array/src/main/java/com/hqeducation/TwoSum.java
@@ -1,5 +1,8 @@
 package com.hqeducation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author dzz
  * @create 2020-11-08 18:28
@@ -8,8 +11,8 @@ package com.hqeducation;
 public class TwoSum {
 
     /*
-    * 1，数组 两数之和 暴力求解
-    * */
+     * 1，数组 两数之和 暴力求解 两数不能是自己必须是两个下标
+     * */
     public static int[] twoSumSimple(int[] nums, int target) {
         int n = nums.length;
         for (int i = 0; i < n; ++i) {
@@ -17,6 +20,22 @@ public class TwoSum {
                 if (nums[i] + nums[j] == target) {
                     return new int[]{i, j};
                 }
+            }
+        }
+        return new int[0];
+    }
+
+    /*
+     * 1，数组 两数之和 利用hash计算 两数不能是自己必须是两个下标 然后 hashmap取得是最后的值 不是第一个
+     * */
+    public static int[] twoSumHash(int[] nums, int target) {
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            hashMap.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{i, hashMap.get(target - nums[i])};
             }
         }
         return new int[0];
--- array/src/test/java/com/hqeducation/TwoSumTest.java
+++ array/src/test/java/com/hqeducation/TwoSumTest.java
@@ -10,9 +10,19 @@ public class TwoSumTest {
     private TwoSum twoSum = new TwoSum();
 
     @Test
-    public void shouldAnswerWithTrue() {
+    public void shouldAnswerWithTrue0() {
         int[] arr1 = new int[]{1,2,3,4,5};
         int [] arr2 = twoSum.twoSumSimple(arr1,6);
+
+        System.out.println(arr2[0]);
+        System.out.println(arr2[1]);
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1() {
+        int[] arr1 = new int[]{3,2,3,4,5};
+        int [] arr2 = twoSum.twoSumHash(arr1,6);
 
         System.out.println(arr2[0]);
         System.out.println(arr2[1]);
